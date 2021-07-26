34fbec246408a116b7c4831333d4828194271571
Emox133 <emir.betaacc0unt@gmail.com>
2021-07-26 14:00:10 +0200

    jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const catchAsync = require('./../utils/catchAsync')
 const AppError = require('./../utils/AppError')
 const User = require('./../models/UserModel')
+const {promisify} = require('util')
+const signToken = require('./../utils/signToken')
 
 exports.signup = catchAsync(async(req, res, next) => {
     const registeredUser = await User.create({
@@ -10,9 +12,11 @@ exports.signup = catchAsync(async(req, res, next) => {
         confirmPassword: req.body.confirmPassword
     })
 
+    const token = signToken(registeredUser._id)
+
     res.status(201).json({
         message: 'success',
-        registeredUser
+        token
     })
 })
 
@@ -29,12 +33,32 @@ exports.login = catchAsync(async(req, res, next) => {
         return next(new AppError('Netačan e-mail ili lozinka.', 401))
     }
 
+    const token = signToken(user._id)
+
     res.status(201).json({
         message: 'success',
-        user
+        token
     })
 })
 
 exports.protectRoutes = catchAsync(async(req, res, next) => {
+    let token
     
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1]
+    }
+
+    if(!token) {
+        return next(new AppError('Neispravan token', 401))
+    }
+
+    const decodedToken = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+    const currentUser = User.findById(decodedToken.id)
+
+    if(!currentUser) {
+        return next(new AppError('Korisnik povezan sa ovim tokenom više ne postoji.', 401))
+    }
+
+    req.user = currentUser
+    next()
 })
